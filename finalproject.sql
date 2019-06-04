17028eef1347d3edf0d5ffaef3f6d0a17a386f12
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-04 21:18:32 +0800

    修改用户表结构

--- finalproject.sql
+++ finalproject.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : YZC
+ Source Server         : MySQL
  Source Server Type    : MySQL
- Source Server Version : 80013
+ Source Server Version : 80015
  Source Host           : localhost:3306
  Source Schema         : finalproject
 
  Target Server Type    : MySQL
- Target Server Version : 80013
+ Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 04/06/2019 20:39:39
+ Date: 04/06/2019 21:17:44
 */
 
 SET NAMES utf8mb4;
@@ -25,9 +25,11 @@ CREATE TABLE `user`  (
   `UserName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Sex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Birth` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Birth` timestamp(6) NOT NULL DEFAULT '1970-01-01 08:00:01.000000',
   `Email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `RegDay` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `RegDay` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `LastLogin` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `coin` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`UserName`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
