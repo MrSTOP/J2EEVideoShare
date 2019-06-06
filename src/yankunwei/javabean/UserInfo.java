da6ea1705f045ed50a7f01d693f671cb383cd76d
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-06 15:37:02 +0800

    为UserInfo添加实用方法

--- src/yankunwei/javabean/UserInfo.java
+++ src/yankunwei/javabean/UserInfo.java
@@ -1,4 +1,115 @@
 package yankunwei.javabean;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+
 public class UserInfo {
+    private String userName;
+    private String sex;
+    private Timestamp birth;
+    private Timestamp regDay;
+    private Timestamp lastLogin;
+    private int coin;
+
+    public UserInfo(String userName, String sex, Timestamp birth, Timestamp regDay, Timestamp lastLogin, int coin) {
+        this.userName = userName;
+        this.sex = sex;
+        this.birth = birth;
+        this.regDay = regDay;
+        this.lastLogin = lastLogin;
+        this.coin = coin;
+    }
+
+    public UserInfo() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Timestamp getBirth() {
+        return birth;
+    }
+
+    public String getStrBirth() {
+        return timestampToStr(birth);
+    }
+
+    public void setBirth(Timestamp birth) {
+        this.birth = birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = strToTimestamp(birth);
+    }
+
+    public Timestamp getRegDay() {
+        return regDay;
+    }
+
+    public String getStrRegDay() {
+        return timestampToStr(regDay);
+    }
+
+    public void setRegDay(Timestamp regDay) {
+        this.regDay = regDay;
+    }
+
+    public void setRegDay(String regDay) {
+        this.regDay = strToTimestamp(regDay);
+    }
+
+    public Timestamp getLastLogin() {
+        return lastLogin;
+    }
+
+    public String getStrLastLogin() {
+        return timestampToStr(lastLogin);
+    }
+
+    public void setLastLogin(Timestamp lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public void setLastLogin(String lastLogin) {
+        this.lastLogin = strToTimestamp(lastLogin);
+    }
+
+    public int getCoin() {
+        return coin;
+    }
+
+    public void setCoin(int coin) {
+        this.coin = coin;
+    }
+
+    private String timestampToStr(String timestamp) {
+        return timestamp.split("\\s+")[0];
+    }
+
+    private String timestampToStr(Timestamp timestamp) {
+        return timestampToStr(timestamp.toString());
+    }
+
+    private Timestamp strToTimestamp(String timestamp) {
+        String[] times = timestamp.split("-");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Integer.valueOf(times[0]),
+                     Integer.valueOf(times[1]),
+                     Integer.valueOf(times[2]),
+                     8, 0, 0);
+        return new Timestamp(calendar.getTimeInMillis());
+    }
 }
