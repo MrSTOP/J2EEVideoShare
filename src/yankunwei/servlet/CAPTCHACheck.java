1c6510e69f81809f48e6e411a6ee8eedab455459
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-07 16:44:08 +0800

    完成响应校验验证码AJAX请求的Servlet

---
+++ src/yankunwei/servlet/CAPTCHACheck.java
@@ -0,0 +1,30 @@
+package yankunwei.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet(name = "CAPTCHACheck", urlPatterns = "/CAPTCHACheck")
+public class CAPTCHACheck extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String CAPTCHASubmit = request.getParameter("CAPTCHA");
+        @SuppressWarnings("unchecked")
+        Map<String, String> CAPTCHAMap = (Map<String, String>) request.getServletContext().getAttribute("CAPTCHAMap");
+        if (CAPTCHAMap != null &&
+                CAPTCHAMap.get(email) != null &&
+                CAPTCHAMap.get(email).equals(CAPTCHASubmit)) {
+            response.getWriter().write("ALLOW");
+        } else {
+            response.getWriter().write("DENY");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
