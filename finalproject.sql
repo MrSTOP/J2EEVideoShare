f0610c377f3ec15b178adca092aeb2a72af02429
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-09 18:47:22 +0800

    删除空格

--- finalproject.sql
+++ finalproject.sql
@@ -1,37 +1,28 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : YZC
+ Source Server         : MySQL
  Source Server Type    : MySQL
- Source Server Version : 80013
+ Source Server Version : 80015
  Source Host           : localhost:3306
  Source Schema         : finalproject
 
  Target Server Type    : MySQL
- Target Server Version : 80013
+ Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 09/06/2019 17:46:55
+ Date: 07/06/2019 14:11:45
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for collection
--- ----------------------------
-DROP TABLE IF EXISTS `collection`;
-CREATE TABLE `collection`  (
-  `uid` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `videoID` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`uid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
+  `UID` int(255) NOT NULL AUTO_INCREMENT,
   `UserName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Sex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -39,18 +30,22 @@ CREATE TABLE `user`  (
   `Email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `RegDay` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `LastLogin` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `coin` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`UserName`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `Coin` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`UID`, `UserName`) USING BTREE,
+  INDEX `UID`(`UID`) USING BTREE,
+  UNIQUE INDEX `Email`(`Email`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for watchhistory
 -- ----------------------------
 DROP TABLE IF EXISTS `watchhistory`;
 CREATE TABLE `watchhistory`  (
-  `UserName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `VideoID` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Date` datetime(0) NULL DEFAULT NULL
+  `UID` int(255) NOT NULL,
+  `VideoID` int(255) NULL DEFAULT NULL,
+  `Date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  INDEX `UID`(`UID`) USING BTREE,
+  CONSTRAINT `watchhistory_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user` (`UID`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
