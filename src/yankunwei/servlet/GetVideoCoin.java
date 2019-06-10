824e0a9c3147c10fe82de1fb7fb3c5b82a33ca63
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-10 15:23:22 +0800

    添加处理获取硬币AJAX请求的Servlet

---
+++ src/yankunwei/servlet/GetVideoCoin.java
@@ -0,0 +1,26 @@
+package yankunwei.servlet;
+
+import yankunwei.javabean.IVideoDAO;
+import yankunwei.javabean.VideoDAO;
+import yankunwei.javabean.VideoInfo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "GetVideoCoin", urlPatterns = "/GetVideoCoin")
+public class GetVideoCoin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String videoID = request.getParameter("VideoID");
+        IVideoDAO videoDAO = new VideoDAO();
+        VideoInfo videoInfo = videoDAO.SelectVideo(videoID);
+        response.getWriter().write("{ \"coin\":" + videoInfo.getCoin() +"}");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
