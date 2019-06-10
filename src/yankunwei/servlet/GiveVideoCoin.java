ce248ccd04953fca29b63ef5a483f94963fc431e
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-10 15:24:01 +0800

    完成响应投硬币的AJAX请求的Servlet

--- src/yankunwei/servlet/GiveVideoCoin.java
+++ src/yankunwei/servlet/GiveVideoCoin.java
@@ -1,13 +1,13 @@
 package yankunwei.servlet;
 
-import yankunwei.javabean.VideoDAO;
-import yankunwei.javabean.VideoInfo;
+import yankunwei.javabean.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 
 @WebServlet(name = "GiveVideoCoin", urlPatterns = "/GiveVideoCoin")
@@ -15,6 +15,9 @@ public class GiveVideoCoin extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String videoID = request.getParameter("VideoID");
         VideoDAO videoDAO = new VideoDAO();
+        IUserInfoDAO userInfoDAO = new UserInfoDAO();
+        UserInfo userInfo = ((UserInfo) request.getSession().getAttribute(LoginCheck.SESSION_ATTR_USER_INFO));
+        userInfoDAO.giveVideoCoin(userInfo, videoID);
         VideoInfo videoInfo = videoDAO.SelectVideo(videoID);
         response.getWriter().write("{ \"coin\":" + videoInfo.getCoin() +"}");
     }
