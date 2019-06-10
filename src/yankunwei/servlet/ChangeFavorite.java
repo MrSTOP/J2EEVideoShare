b62e8996e6b0b01806941c71955534d4fb1834b1
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-06-10 14:20:48 +0800

    完成响应收藏信息AJAX请求的Servlet

--- src/yankunwei/servlet/ChangeFavorite.java
+++ src/yankunwei/servlet/ChangeFavorite.java
@@ -1,9 +1,6 @@
 package yankunwei.servlet;
 
-import yankunwei.javabean.CollectVideoDAO;
-import yankunwei.javabean.ICollectVideoDAO;
-import yankunwei.javabean.UserInfo;
-import yankunwei.javabean.VideoDAO;
+import yankunwei.javabean.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,14 +9,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "GetIsFavorite", urlPatterns = "/GetIsFavorite")
-public class GetIsFavorite extends HttpServlet {
+@WebServlet(name = "ChangeFavorite", urlPatterns = "/ChangeFavorite")
+public class ChangeFavorite extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String videoID = request.getParameter("VideoID");
         int UID = ((UserInfo) request.getSession().getAttribute(LoginCheck.SESSION_ATTR_USER_INFO)).getUID();
         ICollectVideoDAO collectVideoDAO = new CollectVideoDAO();
-        boolean isFaovrite = collectVideoDAO.SelectVideo(videoID, UID);
-        response.getWriter().write("{\"favorite\":" + isFaovrite + "}");
+        IVideoDAO videoDAO = new VideoDAO();
+        String videoName = videoDAO.SelectVideo(videoID).getVideoName();
+        boolean isFavorite = collectVideoDAO.SelectVideo(videoID, UID);
+        if (isFavorite) {
+            collectVideoDAO.deleteVideoCollect(UID, videoID);
+        } else {
+            collectVideoDAO.addVideoCollect(UID, videoID, videoName);
+        }
+        response.getWriter().write("{\"favorite\":" + !isFavorite + "}");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
